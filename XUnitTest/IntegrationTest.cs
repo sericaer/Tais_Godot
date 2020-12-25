2df25db2becc479ef5a3848d106fac03e2b15d21
sericaer <sericaer@outlook.com>
2020-12-25 17:45:36 +0800

    清除告警信息

--- XUnitTest/IntegrationTest.cs
+++ XUnitTest/IntegrationTest.cs
@@ -67,7 +67,9 @@ namespace XUnitTest
 
     public class ClassSource : INotifyPropertyChanged
     {
+#pragma warning disable 0067
         public event PropertyChangedEventHandler PropertyChanged;
+#pragma warning restore 0067
 
         public int data { get; set; }
     }
