487a681f7209b8e463b18b66e4365c74b5d4cb4c
sericaer <sericaer@outlook.com>
2021-01-01 18:05:33 +0800

    修改错误

--- TaisGodot/Global/SaveLoadPanel/FileContainer/SaveFileContainer.cs
+++ TaisGodot/Global/SaveLoadPanel/FileContainer/SaveFileContainer.cs
@@ -6,16 +6,24 @@ namespace TaisGodot.Scripts
 {
 	class SaveFileContainer : ScrollContainer
 	{
+		Container container;
+
+		public override void _Ready()
+		{
+			container = GetNode<VBoxContainer>("VBoxContainer");
+
+		}
+
 		internal IEnumerable<SaveFileItemPanel> EnumSaveFileItems()
 		{
-			return GetNode<VBoxContainer>("VBoxContainer").GetChildren<SaveFileItemPanel>();
+			return container.GetChildren<SaveFileItemPanel>();
 		}
 
 		internal void GenerateFileItems(bool enableLoad, Action<string> onTriggerLoad, Action<string> onTriggerDelete)
 		{
 			foreach (var filePath in System.IO.Directory.EnumerateFiles(GlobalPath.save, "*.save"))
 			{
-				var fileItem = SaveFileItemPanel.Instance(this, enableLoad, filePath);
+				var fileItem = SaveFileItemPanel.Instance(container, enableLoad, filePath);
 				fileItem.actTriggerLoad = onTriggerLoad;
 				fileItem.actTriggerDelete = onTriggerDelete;
 			}
